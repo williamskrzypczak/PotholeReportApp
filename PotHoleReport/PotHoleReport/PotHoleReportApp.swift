c63c865f57faff5773141d3491edaacaddae9420
bskrzypczak <bskrzypczak@mchenry.edu>
2024-04-03 14:52:14 -0500

    1st Commit
    1st Commit

---
+++ PotHoleReport/PotHoleReport/PotHoleReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotHoleReportApp.swift
+//  PotHoleReport
+//
+//  Created by bill on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PotholeReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
